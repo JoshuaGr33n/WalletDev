edfea54da4ce3093817a1d76eaacef61ec9f986f
joshuaGr33n <joshuaoleru@gmail.com>
2022-10-07 18:29:14 +0100

    kk

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+
 use App\Models\User;
 use App\Models\Role;
 use App\Models\Role_user;
 use Illuminate\Support\Facades\Hash;
 use Carbon\Carbon;
+use Illuminate\Support\Str;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -107,36 +109,36 @@ Route::get('/validate_qr_code', 'App\Http\Controllers\Cron\QrvalidateController@
 
 
 Route::get('db', function () {
+    $referral_code = $this->generateUniqueCode();
+    $user = User::create([
+        'first_name' => "John",
+        'last_name' => "Wick",
+        'full_name' => "John Wick",
+        'gender' => "M",
+        'user_name' => 'John' .$referral_code,
+        'role' => 'Customer',
+        'dob' => "1998-02-21",
+        'postal_code' => "10006",
+        'country_code' => "+234",
+        'phone_number' => "7035069200",
+        'referral_code' => "",
+        'email' => "wick@walletapp.com",
+        'password' => "",
+        'registered_date' => Carbon::now()->format('Y-m-d'),
+        'user_unique_id' => Str::uuid(),
+        'member_id' =>'bkk-' . rand()
+    ]);
 
-    // $user = User::create([
-    //     'first_name' => "Admin",
-    //     'last_name' => "John",
-    //     'full_name' => "Admin John",
-    //     'gender' => "M",
-    //     'user_name' => "Admin",
-    //     'role' => 'ADMIN',
-    //     'dob' => "1912-02-13",
-    //     'postal_code' => "10006",
-    //     'country_code' => "+13",
-    //     'phone_number' => "88888888809",
-    //     'referral_code' => "",
-    //     'email' => "admin@walletapp.com",
-    //     'password' => Hash::make('admin'),
-    //     'registered_date' => Carbon::now()->format('Y-m-d'),
-    //     'user_unique_id' => "",
-    //     'member_id' => ""
+
+
+    // $role = Role::create([
+    //     'name' => "Admin",
+    //     'guard_name' => "Web"
     // ]);
 
-
-
-    $role = Role::create([
-        'name' => "Admin",
-        'guard_name' => "Web"
-    ]);
-
-    $role_user = Role_user::create([
-        'user_id' => 1,
-        'role_id' => 1,
-    ]);
+    // $role_user = Role_user::create([
+    //     'user_id' => 1,
+    //     'role_id' => 1,
+    // ]);
 
 });
