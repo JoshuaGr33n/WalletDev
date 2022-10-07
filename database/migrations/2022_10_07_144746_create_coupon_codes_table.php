ac1fef12fcb7495658365228c6c0fc7914f39c31
joshuaGr33n <joshuaoleru@gmail.com>
2022-10-07 17:04:20 +0100

    kkk

--- database/migrations/2022_10_07_144746_create_coupon_codes_table.php
+++ database/migrations/2022_10_07_144746_create_coupon_codes_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCouponCodesTable extends Migration
+class CreateCouponCodesTablee extends Migration
 {
     /**
      * Run the migrations.
