df0efbe24807e96bd325533f5a404235faa0cf25
joshuaGr33n <joshuaoleru@gmail.com>
2022-10-14 07:22:50 +0100

    kk

--- database/migrations/2022_10_14_055627_create_users_table.php
+++ database/migrations/2022_10_14_055627_create_users_table.php
@@ -24,7 +24,7 @@ class CreateUsersTable extends Migration
             $table->string('user_name', 60)->nullable();
             $table->string('referral_code')->nullable();
             $table->string('role')->nullable();
-            $table->bit('donotdisturb', 1)->default(b'0');
+            $table->int('donotdisturb', 11)->default(0);
             $table->date('dob')->nullable();
             $table->string('photo')->nullable();
             $table->string('country_code', 15)->nullable();
