d4590c005ee84d5301f08d8f873a4daca82591d4
joshuaGr33n <joshuaoleru@gmail.com>
2022-10-14 08:47:18 +0100

    ll

--- app/Models/Outlet.php
+++ app/Models/Outlet.php
@@ -10,6 +10,19 @@ class Outlet extends Model
 	protected $primaryKey = 'id';
     protected $table = 'merchant_outlet';
 
+    protected $fillable = [
+        'merchant_id',
+        'outlet_name',
+        'outlet_address' ,
+        'outlet_latitude',
+        'outlet_longitude',
+        'outlet_phone',
+        'outlet_hours',
+        'created_at',
+        'status',
+        
+    ];
+
     public function merchant()
     {
     	return $this->belongsTo('App\Models\Merchant','merchant_id');
