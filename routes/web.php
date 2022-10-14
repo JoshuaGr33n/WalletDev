770632ff5877f26f02301c755456f58075cb551e
joshuaGr33n <joshuaoleru@gmail.com>
2022-10-14 08:20:15 +0100

    kk

--- routes/web.php
+++ routes/web.php
@@ -112,25 +112,25 @@ Route::get('/validate_qr_code', 'App\Http\Controllers\Cron\QrvalidateController@
 
 Route::get('db', function () {
 
-    // $user = User::create([
-    //     'first_name' => "Admin",
-    //     'last_name' => "Admin",
-    //     'full_name' => "Admin Fred",
-    //     'gender' => "M",
-    //     'user_name' => 'Admin',
-    //     'role' => 'ADMIN',
-    //     'dob' => "1912-02-13",
-    //     'postal_code' => "10006",
-    //     'country_code' => "13",
-    //     'phone_number' => "88888888809",
-    //     'referral_code' => "",
-    //     'email' => "admin@walletapp.com",
-    //     'password' => Hash::make('admin'),
-    //     'registered_date' => Carbon::now()->format('Y-m-d'),
-    //     'donotdisturb' => 0,
-    //     "location" => "China",
-    //     "state" => "Shanghai"
-    // ]);
+    $user = User::create([
+        'first_name' => "Admin",
+        'last_name' => "Admin",
+        'full_name' => "Admin Fred",
+        'gender' => "M",
+        'user_name' => 'Admin',
+        'role' => 'ADMIN',
+        'dob' => "1912-02-13",
+        'postal_code' => "10006",
+        'country_code' => "13",
+        'phone_number' => "88888888809",
+        'referral_code' => "",
+        'email' => "admin@walletapp.com",
+        'password' => Hash::make('admin'),
+        'registered_date' => Carbon::now()->format('Y-m-d'),
+        'donotdisturb' => 0,
+        "location" => "China",
+        "state" => "Shanghai"
+    ]);
 
     //  $user = User::find(2);
     // $user->country_code = "234";
@@ -140,18 +140,18 @@ Route::get('db', function () {
 
 
 
-    // $role = Role::create([
-    //     'name' => "Admin",
-    //     'guard_name' => "Web"
-    // ]);
+    $role = Role::create([
+        'name' => "Admin",
+        'guard_name' => "Web"
+    ]);
     // if ($role->save()) {
     //     return response()->json(['res' => 'Successful']);
     // }
 
-    // $role_user = Role_user::create([
-    //     'user_id' => 1,
-    //     'role_id' => 1,
-    // ]);
+    $role_user = Role_user::create([
+        'user_id' => 1,
+        'role_id' => 1,
+    ]);
     // if ($role_user->save()) {
     //     return response()->json(['res' => 'Successful']);
     // }
